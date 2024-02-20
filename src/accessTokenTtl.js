251e31a06c7bc1de82300ab7b633b141ced056c0
Giovanni Beri <giovanni.beri@sinch.com>
2024-02-20 15:41:40 +0100

    Default TTL to 1h

--- src/accessTokenTtl.js
+++ src/accessTokenTtl.js
@@ -4,7 +4,7 @@
  * TTL of access tokens generated in the `POST /oauth/tokens` endpoints;
  * using a separate file to simplify mocking in unit tests.
  */
-const ACCESS_TOKEN_TTL = 3800;
+const ACCESS_TOKEN_TTL = 3600;
 
 module.exports = {
   ACCESS_TOKEN_TTL,
